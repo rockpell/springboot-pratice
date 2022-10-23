1f84b95f101af17423a5bdb199faa85259df4be5
rockpell <aythffk@gmail.com>
2022-10-23 13:10:12 +0900

    Feat: create member v1 api 추가

---
+++ src/main/java/com/example/springbootpractice/controller/MemberApiController.java
@@ -0,0 +1,23 @@
+package com.example.springbootpractice.controller;
+
+import com.example.springbootpractice.domain.Member;
+import com.example.springbootpractice.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long memberId = memberService.join(member);
+
+        return new CreateMemberResponse(memberId);
+    }
+}
