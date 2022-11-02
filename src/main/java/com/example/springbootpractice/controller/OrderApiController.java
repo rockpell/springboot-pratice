ae0a5d57fa0343aba37f6fe36308eb7561fc8bd9
rockpell <aythffk@gmail.com>
2022-11-02 23:49:52 +0900

    Feat: order v3.1, BatchSize
    - distinct를 사용하지 않고 batchSize를 이용해해서 최적화
    - batchSize를 사용하면 join fetch 대상을 in 쿼리로 가져와서 쿼리 최적화가 자동으로 가능해짐

--- src/main/java/com/example/springbootpractice/controller/OrderApiController.java
+++ src/main/java/com/example/springbootpractice/controller/OrderApiController.java
@@ -6,6 +6,7 @@ import com.example.springbootpractice.repository.OrderRepository;
 import com.example.springbootpractice.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,7 +22,7 @@ public class OrderApiController {
      * V1. 엔티티 직접 노출
      * - Hibernate5Module 모듈 등록, LAZY=null 처리
      * - 양방향 관계 문제 발생 -> @JsonIgnore
-    * */
+     * */
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
@@ -45,17 +46,34 @@ public class OrderApiController {
     }
 
     /*
-    * jpa의 distinct 기능을 활용해서 데이터 뻥튀기를 제거
-    * query에서의 distinct는 완전히 일치하는 중복데이터만 지워주기 때문에 별 의미 없고
-    * jpa의 distinct가 동작하면 겹치는 데이터를 묶어서 반환해준다.
-    * 단 페이지네이션이 안되고 메모리에 전부 올려서 원하는 개수만큼 내려주는 문제가 생긴다.
-    * */
+     * jpa의 distinct 기능을 활용해서 데이터 뻥튀기를 제거
+     * query에서의 distinct는 완전히 일치하는 중복데이터만 지워주기 때문에 별 의미 없고
+     * jpa의 distinct가 동작하면 겹치는 데이터를 묶어서 반환해준다.
+     * 단 페이지네이션이 안되고 메모리에 전부 올려서 원하는 개수만큼 내려주는 문제가 생긴다.
+     * */
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithItem();
         List<OrderDto> result = orders.stream()
                 .map(o -> new OrderDto(o))
                 .collect(toList());
+        return result;
+    }
+
+    /**
+     * V3.1 엔티티를 조회해서 DTO로 변환 페이징 고려
+     * - ToOne 관계만 우선 모두 페치 조인으로 최적화
+     * - 컬렉션 관계는 hibernate.default_batch_fetch_size, @BatchSize로 최적화
+     */
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+
         return result;
     }
 }
